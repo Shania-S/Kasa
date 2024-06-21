b3600ac07ae81eda56b16e802808e9d24e24aa14
Shania Sidimana <shaniasidimana1@gmail.com>
2024-06-21 11:37:41 +0200

    Fixed collapsible element

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -4,36 +4,42 @@ import { useState } from "react";
 import "./Collapse.scss";
 
 export function Collapse({ header, content, classCollapse, classContent }) {
-  const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
+  const [display, setDisplay, isDisplayed] = useState(false);
   const contentList = typeof content === "string" ? [content] : content;
+
+  const toggleElement = () => {
+    setDisplay(!display);
+  };
 
   return (
     <div className={`collapsible ${classCollapse} `}>
       <div
         className="header"
         style={{
-          borderRadius: isExpanded && "4px",
+          borderRadius: display && "4px",
         }}
       >
         <span>{header}</span>
         <img
+          onClick={toggleElement}
           className="arrowBack"
           src={arrowBack}
           alt={"arrow back picture"}
-          {...getToggleProps()}
           style={{
-            transform: isExpanded ? "rotate(180deg)" : "rotate(0deg)", // Rotate on click
+            transform: display ? "rotate(180deg)" : "rotate(0deg)", // Rotate on click
             transition: "transform 0.3s, width 0.3s", // Smooth transition
           }}
         />
       </div>
-      <div {...getCollapseProps()}>
-        <ul className={`${classContent}`}>
-          {contentList.map((el, index) => (
-            <li key={index}>{el}</li>
-          ))}
-        </ul>
-      </div>
+      {display && (
+        <div>
+          <ul className={`${classContent}`}>
+            {contentList.map((el, index) => (
+              <li key={index}>{el}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
